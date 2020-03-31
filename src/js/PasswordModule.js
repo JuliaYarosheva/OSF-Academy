a41dbfb01cdda2be7daa9ff3341730cdc91f02e6
Julia <missjuliette92@gmail.com>
2020-03-31 13:57:58 +0300

    Refactoring.

--- src/js/PasswordModule.js
+++ src/js/PasswordModule.js
@@ -1,4 +1,5 @@
 import { elements } from './elements';
+import $ from "jquery";
 
 const PasswordModule = function() {
     // Show Password Button
@@ -24,9 +25,11 @@ const PasswordModule = function() {
     const validatePass = () => {
         const input = elements.passInput[0].value;
         const check = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\\w\\s]).{6,}$/;
+        //const modal = document.getElementById('modalLoginForm');
 
         if (input.match(check)) {
-            alert `You logged in!`;
+
+            $('#modalLoginForm').modal('toggle');
 
         } else {
             alert `Password must contain at least 6 characters, 1 numeric, 1 uppercase letter, 1 special character.`
